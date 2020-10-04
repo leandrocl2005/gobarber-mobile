110812163402cb38db838c80f2b79eeb8c198c32
leandrocl2005 <leandrocl2005@yahoo.com.br>
2020-10-04 14:32:39 -0300

    upload photo

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -12,9 +12,9 @@ import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import Icon from 'react-native-vector-icons/Feather';
+import ImagePicker from 'react-native-image-picker';
 
 import { useAuth } from '../../hooks/auth';
-// import api from '../../services/api';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -126,6 +126,43 @@ const Profile: React.FC = () => {
     [navigation, updateUser]
   );
 
+  const handleUpdateAvatar = useCallback(() => {
+    ImagePicker.showImagePicker(
+    {
+      title: 'Selecione um avatar',
+      cancelButtonTitle: 'Cancelar',
+      takePhotoButtonTitle: 'Use câmera',
+      chooseFromLibraryButtonTitle: 'Escolher da galeria',
+      mediaType: 'photo',
+      maxWidth: 400,
+      maxHeight: 400,
+    },
+    (response) =>
+      {
+        if (response.didCancel) {
+          return;
+        }
+
+        if (response.error) {
+          Alert.alert('Erro ao atualizar seu avatar')
+          return;
+        }
+
+        const data = new FormData();
+
+        data.append('avatar', {
+          type: 'image/jpeg',
+          name: `${user.id}.jpg`,
+          uri: response.uri,
+        });
+
+        api.patch('users/avatar', data).then(apiResponse => {
+          updateUser(apiResponse.data);
+        });
+
+      },
+    );
+  }, [updateUser, user.id]);
 
   const handleGoBack = useCallback(() => {
     navigation.goBack();
@@ -147,7 +184,7 @@ const Profile: React.FC = () => {
               <Icon name="chevron-left" size={24} color="#999591" />
             </BackButton>
 
-            <UserAvatarButton onPress={() => {}}>
+            <UserAvatarButton onPress={handleUpdateAvatar}>
               <UserAvatar source={{uri: user.avatar_url}}/>
             </UserAvatarButton>
 
